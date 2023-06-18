a8b3ee201fd0462bfeb496f3c4b5d49165b9602e
Nazila <127387087+Nazilauk@users.noreply.github.com>
2023-06-18 07:18:26 +0100

    Update calculator_with_memory.py

--- calculator_with_memory.py
+++ calculator_with_memory.py
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------------------------------------------
 #This is a Python code that creates a simple calculator application that asks a user to enter 
 #two numbers and the operation (e.g. +, -, x, etc.) that they’d like to perform on the numbers.
-# Display the answer to the equation. Every equation entered by the user should be written to a text file. 
+#It Display the answer to the equation. Every equation entered by the user should be written to a text file. 
 #Uses defensive programming ,this program  can  handles unexpected events and user inputs.
 #------------------------------------------------------------------------------------------------------------
 
